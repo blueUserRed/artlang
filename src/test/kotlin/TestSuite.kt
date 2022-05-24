c3651d4950c595f81d96f5f069110d1f134e94ec
Simon Berthold <9175@htl.rennweg.at>
2022-05-24 13:30:38 +0200

    TestSuite.kt erweitert: CSV mit Test eingelesen und verarbeitet JavaDoc

--- src/test/kotlin/TestSuite.kt
+++ src/test/kotlin/TestSuite.kt
@@ -1,3 +1,43 @@
-class TestSuite(tests: List<Test>) {
+import java.nio.file.Paths
+import java.util.regex.Pattern
+import kotlin.io.path.readText
 
+/**
+ * @author Simon Berthold
+ *
+ * TestSuite is an array of tests which can all be performed at once.
+ * There are predefined Suites in testSuites.csv which can be selected with byName or byId. Otherwise it is possible to
+ * create custom suites.
+ */
+class TestSuite private constructor(val tests: List<Test>) {
+
+    @Override
+    override fun toString(): String {
+        return this.tests.toString()
+    }
+
+    companion object {
+        private val testSuitesCSV: List<String> = emptyList()
+
+        fun byId(id: Int): TestSuite {
+            return TestSuite(listOf())
+        }
+
+        fun byName(name: String): TestSuite {
+            val testSuitesString = readTestSuitesCSV().map { it[0] }.toTypedArray()
+            val testSuites = testSuitesString.map { Test(it/*TODO filename*/, false) }
+            return TestSuite(testSuites)
+        }
+
+        fun custom(tests: List<Test>): TestSuite {
+            return TestSuite(tests)
+        }
+
+        private fun readTestSuitesCSV(): Array<Array <String>> {
+            val testSuitesCSV: String = Paths.get("testSuites.csv").readText()
+            println(testSuitesCSV)
+            val lines = testSuitesCSV.split(Regex(Pattern.quote("\n"))).toTypedArray()
+            val values = lines.forEach { s: String -> s.split(";") }
+        }
+    }
 }
--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -1,37 +1,43 @@
 import errors.ErrorPool
 import java.io.File
 import java.nio.file.Paths
-import kotlin.io.path.readLines
 import kotlin.io.path.readText
 
 fun main() {
     val test = Test("FizzBuzz.art", false)
 //    val test = Test("HelloWorld.art")
     test.test()
+
+    val ts: TestSuite = TestSuite.byId(0)
+    println(ts.toString())
 }
 
-class Test(private val testfileName: String, private val printOutput: Boolean = true) {
+/**
+ * Test a single .art file form WORKING_DIR/src which has an equally named sample output in WORKING_DIR/sampleOutputs
+ * The .art file gets compiled, then it is run and the output is compared to the sample output.
+ * The meanwhile generated .jar file is placed in WORKING_DIR/out
+ */
+class Test(private val testFileName: String, private val printOutput: Boolean = true) {
 
     private var sampleOutput = try {
-        Paths.get("$sampleOutDir/$testfileName").readText()
+        Paths.get("$sampleOutDir/$testFileName").readText()
     } catch (e: java.nio.file.NoSuchFileException) {
         println("The sample output for this test does not exits. It can therefore not be compared with the programs output.")
         null
     }
-    private var succeeded: Boolean = false;
+    private var succeeded: Boolean = false
 
     fun test() {
-        val args = arrayOf("compile", "$srcDir$testfileName")
+        val args = arrayOf("compile", "$srcDir$testFileName")
         Main.main(args)
         val output = runProgram()
         println()
         if (printOutput) println(output)
         if (sampleOutput == output && !ErrorPool.hasErrors()) {
-            println(Ansi.green + "$testfileName => Test succeeded [\u2713]")
-            succeeded = true;
+            println(Ansi.green + "$testFileName => Test succeeded [\u2713]")
+            succeeded = true
         } else {
-            println(Ansi.red + "$testfileName => Test failed [\u2718]")
-            // TODO compare is to should - Output
+            println(Ansi.red + "$testFileName => Test failed [\u2718]")
             println("Program Output: «$output»")
             if (sampleOutput != null) println("Sample Output: «$sampleOutput»") else {
                 println("Sample output does not exist.")
@@ -41,8 +47,7 @@ class Test(private val testfileName: String, private val printOutput: Boolean = 
     }
 
     private fun runProgram(): String {
-        //TODO: warum ist pwd …/res/out/
-        val jarFile = testfileName.split(".")[0] + ".jar"
+        val jarFile = testFileName.split(".")[0] + ".jar"
         println(jarFile)
         val builder = ProcessBuilder("java", "-jar", jarFile)
         builder.directory(File(outDir))
