fcd83e5fb4d997533f9cf928ff7d016dbffe3265
Marvin Kurka <8013@htl.rennweg.at>
2022-06-08 16:55:08 +0200

    cleaned up test-output, fixed test crashing if they cant execute the .jar, added support for test that are supposed to fail, the format of testSuites.onj is now checked

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -1,18 +1,19 @@
 import errors.ErrorPool
 import java.io.File
+import java.lang.Exception
 import java.nio.file.Paths
 import kotlin.io.path.readText
 
 fun main() {
     val ts = TestSuite.byName("Full")
-    recordOutputs(ts)
-//    ts.run()
+//    recordOutputs(ts)
+    ts.run()
 }
 
 fun recordOutputs(suite: TestSuite) {
-    for (test in suite.tests) {
+    for (test in suite.tests) if (test.outputFielName != null) {
         test.test()
-        File("sampleOutputs/${test.testFileName}").writeText(test.programOutput ?: "")
+        File("sampleOutputs/${test.outputFielName}").writeText(test.programOutput ?: "")
     }
 }
 
@@ -21,57 +22,109 @@ fun recordOutputs(suite: TestSuite) {
  * The .art file gets compiled, then it is run and the output is compared to the sample output.
  * The meanwhile generated .jar file is placed in WORKING_DIR/out
  */
-class Test(val testFileName: String, private val printOutput: Boolean = true) {
+class Test(
+    val testFileName: String,
+    val outputFielName: String?,
+    val expectCompileFailure: Boolean,
+    val expectRuntimeFailure: Boolean,
+    private val printOutput: Boolean = false
+) {
 
     private var hasBeenTested = false
 
-    private var sampleOutput = try {
-        Paths.get("$sampleOutDir/$testFileName").readText()
-    } catch (e: java.nio.file.NoSuchFileException) {
-        println("The sample output for this test does not exits. It can therefore not be compared with the programs output.")
-        null
+    private val sampleOutput: String? = run {
+        if (outputFielName == null) null else try {
+            Paths.get("$sampleOutDir/$outputFielName").readText()
+        } catch(e: java.nio.file.NoSuchFileException) {
+            println("Unable to read file $outputFielName")
+            null
+        }
     }
 
     var succeeded: Boolean = false
 
     var programOutput: String? = null
 
+    init {
+        if (expectCompileFailure && expectRuntimeFailure) {
+            throw IllegalArgumentException("cant expect compiletime and runtime failure simultaneously")
+        }
+    }
+
     fun test() {
+
+        File(testFileName.split(".")[0] + ".jar").delete() //TODO: good look with filenames that don't contain a dot
+
+        println("---------------------------------------------")
+        println("testing: $testFileName")
+        if (expectCompileFailure) println("Expecting compilation to fail")
+        if (expectRuntimeFailure) println("Expecting program to fail at runtime")
+
         val args = arrayOf("compile", "$srcDir$testFileName")
-        File(testFileName.split(".")[0] + ".jar").delete()
         ErrorPool.clear()
         Main.main(args)
-        programOutput = runProgram()
+        val hadCompileFailures = ErrorPool.hasErrors()
+
+        programOutput = if (hadCompileFailures) null else runProgram()
+
         println()
         if (printOutput) println(programOutput)
-        if (sampleOutput?.replace("\r", "") /* I love Windows */ == programOutput && !ErrorPool.hasErrors()) {
-            println(Ansi.green + "$testFileName => Test succeeded [\u2713]")
+
+        val outputsMatch = sampleOutput?.replace("\r", "") /* I love Windows */ == programOutput
+
+        if (hadCompileFailures && expectCompileFailure) {
+            println("${Ansi.green}$testFileName => Test succeeded [\u2713]${Ansi.reset}")
+            succeeded = true
+        } else if (!hadCompileFailures && expectRuntimeFailure && programOutput == null) {
+            println("${Ansi.green}$testFileName => Test succeeded [\u2713]${Ansi.reset}")
+            succeeded = true
+        } else if (!expectRuntimeFailure && !expectCompileFailure && outputsMatch && programOutput != null) {
+            println("${Ansi.green}$testFileName => Test succeeded [\u2713]${Ansi.reset}")
             succeeded = true
         } else {
-            println(Ansi.red + "$testFileName => Test failed [\u2718]")
-            println("Program Output: «$programOutput»")
-            if (sampleOutput != null) println("Sample Output: «$sampleOutput»") else {
+            println("${Ansi.red}$testFileName => Test failed [\u2718]${Ansi.reset}")
+            println("Program Output: «\n$programOutput\n»")
+            if (sampleOutput != null) println("Sample Output: «\n$sampleOutput\n»") else {
                 println("Sample output does not exist.")
             }
         }
-        println(Ansi.reset)
+
+//        if (
+//            outputsMatch &&
+//            !ErrorPool.hasErrors() &&
+//            sampleOutput != null
+//        ) {
+//            println("${Ansi.green}$testFileName => Test succeeded [\u2713]${Ansi.reset}")
+//            succeeded = true
+//        } else {
+//            println("${Ansi.red}$testFileName => Test failed [\u2718]${Ansi.reset}")
+//            println("Program Output: «\n$programOutput\n»")
+//            if (sampleOutput != null) println("Sample Output: «\n$sampleOutput\n»") else {
+//                println("Sample output does not exist.")
+//            }
+//        }
+        println("---------------------------------------------")
         hasBeenTested = true
     }
 
-    private fun runProgram(): String {
-        val jarFile = testFileName.split(".")[0] + ".jar"
-        println(jarFile)
-        val builder = ProcessBuilder("java", "-jar", jarFile)
-        builder.directory(File(outDir))
+    private fun runProgram(): String? {
+        try {
+            val jarFile = testFileName.split(".")[0] + ".jar"
+            println("running: $jarFile")
+            val builder = ProcessBuilder("java", "-jar", jarFile)
+            builder.directory(File(outDir))
 
-        builder.redirectError(ProcessBuilder.Redirect.INHERIT)
+            builder.redirectError(ProcessBuilder.Redirect.INHERIT)
 
-        val process = builder.start()
-        val output = java.lang.StringBuilder()
-        process.inputStream.bufferedReader(Charsets.UTF_8).forEachLine { output.append(it).append("\n") }
-        process.waitFor()
-        if (process.exitValue() != 0) throw RuntimeException("running jar file failed")
-        return output.toString()
+            val process = builder.start()
+            val output = java.lang.StringBuilder()
+            process.inputStream.bufferedReader(Charsets.UTF_8).forEachLine { output.append(it).append("\n") }
+            process.waitFor()
+            if (process.exitValue() != 0) return null
+            return output.toString()
+        } catch (e: Exception) { //TODO: figure out better exception to catch
+            return null
+        }
     }
 
 
